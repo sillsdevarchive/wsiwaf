7e2078998f62d39704dac26c1d0969e0828e69bf
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-09-26 09:03:39 +0000

    docstrings

--- waflib/TaskGen.py
+++ waflib/TaskGen.py
@@ -21,7 +21,7 @@ Additionally, task_gen provides the method "process_source"
 WARNING: subclasses must reimplement the clone method
 """
 
-import traceback, copy, re
+import copy, re
 from waflib import Task, Utils, Logs, Errors
 
 feats = Utils.defaultdict(set)
@@ -501,5 +501,6 @@ class subst_pc(Task.Task):
 
 @extension('.pc.in')
 def add_pcfile(self, node):
+	""".pc.in -> .pc + install the .pc file into ${PREFIX}/lib/pkgconfig/"""
 	tsk = self.create_task('subst_pc', node, node.change_ext('.pc'))
 	self.bld.install_files('${PREFIX}/lib/pkgconfig/', tsk.outputs)
