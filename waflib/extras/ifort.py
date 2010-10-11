145322c93664dfaf936e9e22a10e038a58767562
kwmsmith <kwmsmith@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-11 21:25:26 +0000

    added win32 and cygwin support for fortran compilers (untested)

--- waflib/extras/ifort.py
+++ waflib/extras/ifort.py
@@ -19,6 +19,17 @@ def ifort_modifier_win32(conf):
 	raise NotImplementedError("Ifort on win32 not yet implemented")
 
 @conf
+def ifort_modifier_cygwin(conf):
+	raise NotImplementedError("Ifort on cygwin not yet implemented")
+
+@conf
+def ifort_modifier_platform(conf):
+	dest_os = conf.env['DEST_OS'] or Utils.unversioned_sys_platform()
+	ifort_modifier_func = globals().get('ifort_modifier_' + dest_os)
+	if ifort_modifier_func:
+			ifort_modifier_func(conf)
+
+@conf
 def get_ifort_version(conf, fc):
 	"""get the compiler version"""
 
@@ -38,3 +49,4 @@ def configure(conf):
 	conf.find_ifort()
 	conf.find_ar()
 	conf.fc_flags()
+	conf.ifort_modifier_platform()
