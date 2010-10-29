3a01290400ec64de8d3199fe1aa24ef5ca6bdb11
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-29 23:32:54 +0000

    docs

--- demos/java/animals/junit/org/example/AnimalTest.java
+++ demos/java/animals/junit/org/example/AnimalTest.java
@@ -1,12 +1,9 @@
+package org.example;
 
 import junit.framework.TestCase;
 
 class AnimalTest extends TestCase {
-
 	public void testAnimal() {
-
 		System.out.println("Test run successfully!");
-
 	}
-
 }
